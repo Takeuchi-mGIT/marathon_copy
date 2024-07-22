929649b93c57b3269901e8e45eeab930f49bdb3b
takeuchi_mac_git <hello.tra.takeuchi@gmail.com>
2024-07-23 02:05:42 +0900

    topボタンをエントリーボタンに変更

--- js/script.js
+++ js/script.js
@@ -5,18 +5,23 @@ window.onscroll = function() {
 
 // スクロール位置に応じてボタンを表示・非表示にする関数
 function scrollFunction() {
-    const topBtn = document.getElementById("topBtn");
+    const entryBtn = document.getElementById("entryBtn");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topBtn.style.display = "block"; // ボタンを表示
+        entryBtn.style.display = "block"; // ボタンを表示
         setTimeout(() => {
-            topBtn.style.opacity = "1"; // 徐々に表示
+            entryBtn.style.opacity = "1"; // 徐々に表示
         }, 10); // 非表示から表示までの遅延
     } else {
-        topBtn.style.opacity = "0"; // 徐々に非表示
+        entryBtn.style.opacity = "0"; // 徐々に非表示
         setTimeout(() => {
-            topBtn.style.display = "none"; // 完全に非表示
+            entryBtn.style.display = "none"; // 完全に非表示
         }, 500); // アニメーション終了後に非表示にする遅延
     }
+}
+
+// エントリーにリダイレクトする関数
+function entryFunction() {
+    window.location.href = "https://forms.gle/hbs6B3RTNBuG8qZE9";
 }
 
 // ページトップに戻る関数
