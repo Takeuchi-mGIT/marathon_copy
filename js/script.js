adc6666b9dbff25d622f85121a9584f06f6ee67a
takeuchi_mac_git <hello.tra.takeuchi@gmail.com>
2024-07-23 01:51:01 +0900

    ボタンの微調整

--- js/script.js
+++ js/script.js
@@ -7,9 +7,15 @@ window.onscroll = function() {
 function scrollFunction() {
     const topBtn = document.getElementById("topBtn");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topBtn.style.display = "block";
+        topBtn.style.display = "block"; // ボタンを表示
+        setTimeout(() => {
+            topBtn.style.opacity = "1"; // 徐々に表示
+        }, 10); // 非表示から表示までの遅延
     } else {
-        topBtn.style.display = "none";
+        topBtn.style.opacity = "0"; // 徐々に非表示
+        setTimeout(() => {
+            topBtn.style.display = "none"; // 完全に非表示
+        }, 500); // アニメーション終了後に非表示にする遅延
     }
 }
 
