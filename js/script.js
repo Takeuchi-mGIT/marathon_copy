f82bc0ae63a96389b4b6065bb2dc774261957f7e
takeuchi_mac_git <hello.tra.takeuchi@gmail.com>
2024-07-26 02:12:23 +0900

    エントリーボタン修正

--- js/script.js
+++ js/script.js
@@ -1,32 +1,21 @@
-// スクロールイベントを追加
-window.onscroll = function() {
-    scrollFunction();
-};
+window.onscroll = function() {scrollFunction()};
 
-// スクロール位置に応じてボタンを表示・非表示にする関数
 function scrollFunction() {
-    const entryBtn = document.getElementById("entryBtn");
+    var entryBtn = document.getElementById("entryBtn");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        entryBtn.style.display = "block"; // ボタンを表示
-        setTimeout(() => {
-            entryBtn.style.opacity = "1"; // 徐々に表示
-        }, 10); // 非表示から表示までの遅延
+        entryBtn.style.display = "block";
     } else {
-        entryBtn.style.opacity = "0"; // 徐々に非表示
-        setTimeout(() => {
-            entryBtn.style.display = "none"; // 完全に非表示
-        }, 500); // アニメーション終了後に非表示にする遅延
+        entryBtn.style.display = "none";
     }
 }
 
-// エントリーにリダイレクトする関数
 function entryFunction() {
-    window.location.href = "https://forms.gle/hbs6B3RTNBuG8qZE9";
+    window.location.href = 'https://forms.gle/hbs6B3RTNBuG8qZE9'; // エントリーページに遷移
 }
 
-// ページトップに戻る関数
 function topFunction() {
-    $('html, body').animate({ scrollTop: 0 }, 'slow');
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 // スムーズスクロールの関数
